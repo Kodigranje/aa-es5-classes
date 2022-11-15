a7b1fd4ab27f4f26eaa7489dee7a99a572fb7070
Kodigranje <coachell001@gmail.com>
2022-11-15 16:04:04 +0100

    ES6 to ES5 syntax (Person class): done

--- classes/person.js
+++ classes/person.js
@@ -1,25 +1,43 @@
 const Dog = require('./dog');
 
-class Person {
-  constructor(name, age, dogs = []) {
-    this.name = name;
-    this.age = age;
-    this.dogs = dogs;
-  }
-
-  addDog(dog) {
-    if (!(dog instanceof Dog)) {
-      throw new TypeError("Can only add dogs!");
-    }
-    this.dogs.push(dog);
-  }
-
-  walkDogs() {
-    this.dogs.forEach((dog) => {
-      console.log(`walking ${dog.name}`);
-    });
-  }
+// ES5
+function Person (name, age, dogs = []) {
+  this.name = name;
+  this.age = age;
+  this.dogs = dogs;
 }
+
+Person.prototype.addDog = function (dog) {
+  if(!(dog instanceof Dog)) throw new TypeError('Can only add dogs!');
+  // this: instance of Person - Alex, age:31...
+  this.dogs.push(dog);
+};
+
+Person.prototype.walkDogs = function() {
+  this.dogs.forEach(dog => console.log(`walking ${dog.name}`));
+}
+
+// ES6
+// class Person {
+//   constructor(name, age, dogs = []) {
+//     this.name = name;
+//     this.age = age;
+//     this.dogs = dogs;
+//   }
+
+//   addDog(dog) {
+//     if (!(dog instanceof Dog)) {
+//       throw new TypeError("Can only add dogs!");
+//     }
+//     this.dogs.push(dog);
+//   }
+
+//   walkDogs() {
+//     this.dogs.forEach((dog) => {
+//       console.log(`walking ${dog.name}`);
+//     });
+//   }
+// }
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
