5f12e2132a257dfbaa8b4893e2646eeaf075def5
Kodigranje <coachell001@gmail.com>
2022-11-15 09:50:36 +0100

    ES5 adding instance methods: needs debugging

--- classes/dog.js
+++ classes/dog.js
@@ -1,17 +1,30 @@
-class Dog {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  bark() {
-    console.log("Bark");
-  }
-
-  static diet() {
-    return "carnivore";
-  }
+// ES5 01
+function Dog (name, age) {
+  this.name = name;
+  this.age = age;
 }
+/** DEBUG **********************************************************
+ * 1) should print 'Bark' to the console
+ *******************************************************************/
+
+Dog.prototype.bark = function () { console.log('bark'); };
+Dog.diet = function() { return 'carnivore'; };
+
+// ES6
+// class Dog {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   bark() {
+//     console.log("Bark");
+//   }
+
+//   static diet() {
+//     return "carnivore";
+//   }
+// }
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
